09b86d7e60798c7f481d0441988376721e58b8d6
hi2o2 <112685702+hi2o2@users.noreply.github.com>
2022-10-16 17:56:44 +0530

    Create third.java

---
+++ third.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class third
+{
+public static void main (String args[])
+{
+Scanner  obj=new Scanner(System.in);
+System.out.println("Enter the value of a");
+int a =obj.nextInt();
+if (a%2==0)
+	System.out.println(a+"is even");
+else
+	System.out.println(a+"is odd");
+}
+}
