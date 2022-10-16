a8ce9856bf25668dc49d6d34af04fa671125ccfd
hi2o2 <112685702+hi2o2@users.noreply.github.com>
2022-10-16 17:05:08 +0530

    Create second.java

---
+++ second.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class second
+{
+public static void main (String args[])
+{
+Scanner  obj=new Scanner(System.in);
+System.out.println("Enter the value of a");
+int a =obj.nextInt();
+System.out.println("Enter the value of b");
+int b =obj.nextInt();
+int c=a+b;
+System.out.println("Sum ="+c);
+}
+}
